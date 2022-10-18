fb80e3f202475c3dc1ec90bcb102235c17f20801
SalimMertTas <tassalim000@gmail.com>
2022-10-18 23:00:15 +0300

    first-js-end-commit

---
+++ first.js
@@ -0,0 +1,13 @@
+let username = prompt("lütfen kulanici adinizi giriniz.")
+let text = document.querySelector("#info")
+
+function tarihSaat() {
+    let date = new Date().toLocaleString();
+    document.getElementById("date-id").innerHTML = date;
+}
+setInterval(tarihSaat , 1000);
+
+
+text.innerHTML = ` <strong>${username}</strong> Hoşgeldin! 
+<strong>
+Kodluyoruz Frontend Web Development Patikası</strong>'nın <strong>Javascript bölümü</strong> 1. Ödevindesin!`
